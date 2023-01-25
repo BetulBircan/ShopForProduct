bcc196da5b2a8609995ee10e16f6da06a5343d25
BetulBircan <bircan.betul_1996@hotmail.com>
2023-01-26 02:48:10 +0300

    düzenleme yapıldı

--- shopproduct-app/src/views/HomePage.vue
+++ shopproduct-app/src/views/HomePage.vue
@@ -8,11 +8,7 @@
 
 <script>
 
-<<<<<<< HEAD
-import RelatedItemSection from '../components/Section/RelatedItemSection.vue'
-=======
 import RelatedItemSection from '../components/Section/RelatedItemsSection'
->>>>>>> 9c0f8b4c1090466ab1fb93f0c9887bfc41e3f868
 
 export default {
     components : {
