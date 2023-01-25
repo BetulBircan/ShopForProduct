75f6b59a03bdeacd1df9d65845ebf6b42da125e0
BetulBircan <bircan.betul_1996@hotmail.com>
2023-01-26 00:32:20 +0300

    yönlendirmeler için anasayfa,hakkında ürünler vs oluşturuldu

---
+++ shopproduct-app/src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>Anasayfa</h3>
+
+    <RelatedItemsSection />
+
+
+</template>
+
+<script>
+
+import RelatedItemsSection from '../components/Section/RelatedItemsSection'
+
+export default {
+    components : {
+        RelatedItemsSection
+    }
+}
+</script>
