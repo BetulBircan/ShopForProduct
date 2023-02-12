57330fd78e765ab5b72b793f1874b7ab232ca663
BetulBircan <bircan.betul_1996@hotmail.com>
2023-02-12 04:38:20 +0300

    footer düzeltildi

--- shopproduct-app/src/components/PageContent/FooterComponent.vue
+++ shopproduct-app/src/components/PageContent/FooterComponent.vue
@@ -1,9 +1,14 @@
 <template>
     <footer class="py-5 bg-dark">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+    <div><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
 </footer>
 </template>
 
 <style scoped>
-
+footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+}
 </style>
