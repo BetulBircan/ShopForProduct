4847bcc4021b2ffdaa8115bf712dcdeb7e53bc22
BetulBircan <bircan.betul_1996@hotmail.com>
2023-02-14 23:25:33 +0300

    ...

--- shopproduct-app/src/components/Section/AllProductsSection.vue
+++ shopproduct-app/src/components/Section/AllProductsSection.vue
@@ -117,7 +117,7 @@ export default {
     box-shadow: 0 2px 1px -1px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
     border-radius: 4px;
     margin-bottom: 2rem;
-    max-width: 100%
+    width: 100%
 }
 
 .p-dropdown {
@@ -168,8 +168,8 @@ export default {
 
     .product-grid-item-content {
         text-align: center;
-        max-width: 353px;
-        max-height: 403px;
+        width: 353px;
+        height: 403px;
         margin: 7px;
         padding: 28px;
     }
